0a650c5cf517f4ec3fc9fdd9c1d600c15d866dad
A. Bojkov <49661236+abojkov@users.noreply.github.com>
2020-05-03 00:04:00 +0200

    v1.0

---
+++ assets/js/myScript.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+   $(".error").after("<h5 style='color: red;'>Во моментот е невозможно да го слушате радиото</h5>");
+   $(".error").remove();
+
+   $(".outofchrome").after("<h5 style='color: red;'>Доколку користите Google Chrome, можно е да имате проблеми при вчитувањето на содржината</h5>");
+});
