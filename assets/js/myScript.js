2c2e276843790155f7e8fc6faed8751b0154dcaa
Aleksandar Bojkov <aleksandar.bojkov@netcetera.com>
2021-06-30 18:57:08 +0200

    v2.0

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-   $(".error").after("<h5 style='color: red;'>Во моментот е невозможно да го слушате радиото</h5>");
+   $(".error").after("<h5 style='color: red;'>You can not listen to this radio station at the moment. Sorry for the inconvenience.</h5>");
    $(".error").remove();
 
-   $(".outofchrome").after("<h5 style='color: red;'>Доколку користите Google Chrome, можно е да имате проблеми при вчитувањето на содржината</h5>");
+   $(".outofchrome").after("<h5 style='color: red;'>If you are using Google Chrome, you may have issues with loading the data.</h5>");
 });
